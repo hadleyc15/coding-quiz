4eb42daffd8b38b51384505491482bcb158a28b4
Chris Hadley <hadleyc15@yahoo.com>
2020-05-22 09:27:58 -0600

    added the quiz questions to the js file

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+// list of all questions, choices, and answers
+var questions = [
+    {
+        Question: "What does HTML stand for?",
+        choices: ["Hyper Text Makeup Language", "HyperText Markup Language", "HyperText Madeup Language", "Habanero Tacos Made Lovingly"],
+        answer: "HyperText Markup Language"
+    },
+    {
+        Question: "If we want to wrap a block of text around an image, which css property will we use?",
+        choices: ["wrap", "push", "float", "align"],
+        answer: "float"
+    },
+    {
+        Question: "Arrays in JavaScript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        Question:"What does CSS stand for?",
+        choices: ["Complete Styling Sheets", "Cascading Simple Style", "Complex Style Simulator", "Cascading Style Sheets"],
+        answer: "Cascading Style Sheets"
+    },
+    {
+        Question:"A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+        answer: "console.log"
+    },
+    {
+        Question:"BONUS QUESTION!! What is the best NFL team according to the person who wrote this quiz??",
+        choices: ["Green Bay Packers", "Dallas Cowboys", "San Fransisco 49ers", "Arizona Cardinals"],
+        answer: "Arizona Cardinals"
+    },
+];
