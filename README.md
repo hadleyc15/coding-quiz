3b7af251f159fa368877ca5b101c98ba5f67f269
Chris Hadley <hadleyc15@yahoo.com>
2020-05-22 20:30:09 -0600

    todays progress

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# coding-quiz
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and score
+
+This assignment was very hard for me.  The html and css came pretty easy.  The javascript was a different story.  I created an html document to mock the example given, with a few changes of my own.  I had thought about putting the highscores and timer in a header, but eventually decided against it mostly because i couldn't get the css to function correctly with it when I had it as a header.  So I created a <div> with its own id to house these elements.  
+
+The most fun in this assignment was in the css.  I really enjoyed playing around with the style and trying to figure out exactly how I wanted it to go.  I created a root css property (after i remembered that was a thing), to house my two main color schemes....not gonna lie, totally forgot about it until I had already copied my primary and secondary colors throughout my entire css....oops...probably the most fun I had was in designing the buttons and having them chenge colors when you hover over them.  
+
+The Javascript was a whole other beast.  Javascript is not something I understand very well so I ended up googling every function and everything i could think of that i needed to make it do and using what I found through my searches.  On top of that many phone calls with friends and a family member who are already developers I was able to sort out my Javascript.  
--- assets/js/script.js
+++ assets/js/script.js
@@ -11,49 +11,43 @@ var userScores = []
 
 // list of all questions, choices, and answers
 var questions = [
-    {
-        //Question 1
-        Question: "What does HTML stand for?",
-        choices: ["Hyper Text Makeup Language", "HyperText Markup Language", "HyperText Madeup Language", "Habanero Tacos Made Lovingly"],
-        answer: "HyperText Markup Language"
-    },
-    {
-        //Question 2
-        Question: "If we want to wrap a block of text around an image, which css property will we use?",
-        choices: ["wrap", "push", "float", "align"],
-        answer: "float"
-    },
-    {
-        //Question 3
-        Question: "Arrays in JavaScript can be used to store ____.",
-        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-        answer: "all of the above"
-    },
-    {
-        //Question 4
-        Question: "What does CSS stand for?",
-        choices: ["Complete Styling Sheets", "Cascading Simple Style", "Complex Style Simulator", "Cascading Style Sheets"],
-        answer: "Cascading Style Sheets"
-    },
-    {
-        //Questiopn 5
-        Question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
-        answer: "console.log"
-    },
-    {
-        //Question 6
-        Question: "BONUS QUESTION!! What is the best NFL team according to the person who wrote this quiz??",
-        choices: ["Green Bay Packers", "Dallas Cowboys", "San Fransisco 49ers", "Arizona Cardinals"],
-        answer: "Arizona Cardinals"
-    },
+	{
+		Question: "What does HTML stand for?",
+		choices: ["Hyper Text Makeup Language", "HyperText Markup Language", "HyperText Madeup Language", "Habanero Tacos Made Lovingly"],
+		answer: "HyperText Markup Language"
+	},
+	{
+		Question: "If we want to wrap a block of text around an image, which css property will we use?",
+		choices: ["wrap", "push", "float", "align"],
+		answer: "float"
+	},
+	{
+		Question: "Arrays in JavaScript can be used to store ____.",
+		choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+		answer: "all of the above"
+	},
+	{
+		Question:"What does CSS stand for?",
+		choices: ["Complete Styling Sheets", "Cascading Simple Style", "Complex Style Simulator", "Cascading Style Sheets"],
+		answer: "Cascading Style Sheets"
+	},
+	{
+		Question:"A very useful tool used during development and debugging for printing content to the debugger is:",
+		choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+		answer: "console.log"
+	},
+	{
+		Question:"BONUS QUESTION!! What is the best NFL team according to the person who wrote this quiz??",
+		choices: ["Green Bay Packers", "Dallas Cowboys", "San Fransisco 49ers", "Arizona Cardinals"],
+		answer: "Arizona Cardinals"
+	},
 ];
 
-var pageContentEl = function(element) {
+var pageContentEl = function (element) {
 	return document.querySelector(element);
 };
-//Function for timer, quiz continues if time is > 0.
-var myTimer = function() {
+
+var timer = function () {
 	if (time > 0) {
 		time = time - 1;
 		pageContentEl('#time').innerHTML = time;
@@ -63,41 +57,150 @@ var myTimer = function() {
 		onlyDisplaySection("#finish");
 	}
 };
-//function so that only the things i want displayed are visible during the quiz
-var onlyDisplaySection = function(element) {
+//Only displays what I want to be seen during the quiz
+var onlyDisplaySection = function (element) {
 	let sections = document.querySelectorAll("section");
-	Array.from(sections).forEach(function(userItem) {
+	Array.from(sections).forEach(function (userItem) {
 		userItem.classList.add('hide');
 	});
 	pageContentEl(element).classList.remove('hide');
 };
-//Function for the answer alert to be visible upon answering
-var quizUpdate = function(answerCopy) {
+
+var quizUpdate = function (answerCopy) {
 	pageContentEl('#scoreAlert p').innerHTML = answerCopy;
 	pageContentEl('#scoreAlert').classList.remove('invisible', scoreAlert());
-	Array.from(answers).forEach(answer =>
-	{
+	Array.from(answers).forEach(answer => {
 		answer.classList.add('disable');
-    });
-
-var quizUpdate = function(answerCopy) {
-    pageContentEl('#scoreAlert p').innerHTML = answerCopy;
-    pageContentEl('#scoreAlert').classList.remove('invisible', scoreAlert());
-    Array.from(answers).forEach(answer =>
-    {
-        answer.classList.add('disable');
-    });
-//checks to see timer, if more time, moves on to next question
-    setTimeout(function() {
-        if (quizCount === questions.length) {
-            onlyDisplaySection("#finish");
-            time = 0;
-            pageContentEl('#time').innerHTML = time;
-        } else {
-            setQuestionData();
-            Array.from(answers).forEach(answer => {
-                answer.classList.remove('disable');
-            });
-        }
-    }, 1000);
+	});
+	//If there are questions and time left, test continues, if either run out, test is done
+	setTimeout(function () {
+		if (quizCount === questions.length) {
+			onlyDisplaySection("#finish");
+			time = 0;
+			pageContentEl('#time').innerHTML = time;
+		} else {
+			setQuestion();
+			Array.from(answers).forEach(answer => {
+				answer.classList.remove('disable');
+			});
+		}
+	}, 1000);
 };
+//sets the question as well as the answers associated with that question
+var setQuestion = function () {
+	pageContentEl('#quizHolder p').innerHTML = questions[quizCount].Question;
+	pageContentEl('#quizHolder button:nth-of-type(1)').innerHTML = `1. ${questions[quizCount].choices[0]}`;
+	pageContentEl('#quizHolder button:nth-of-type(2)').innerHTML = `2. ${questions[quizCount].choices[1]}`;
+	pageContentEl('#quizHolder button:nth-of-type(3)').innerHTML = `3. ${questions[quizCount].choices[2]}`;
+	pageContentEl('#quizHolder button:nth-of-type(4)').innerHTML = `4. ${questions[quizCount].choices[3]}`;
+};
+
+var scoreAlert = function () {
+	clearTimeout(setTime);
+	setTime = setTimeout(function () {
+		pageContentEl('#scoreAlert').classList.add('invisible');
+	}, 1000);
+};
+
+var errorAlert = function () {
+	clearTimeout(setTime);
+	setTime = setTimeout(function () {
+		pageContentEl('#errorAlert').classList.add('invisible');
+	}, 3000);
+};
+//Allows user to enter initals at end of quiz
+var enterInitials = function () {
+	let initialsRecord = pageContentEl('#initials').value;
+	if (initialsRecord === '') {
+		pageContentEl('#errorAlert p').innerHTML = "You must enter your initials!";
+		pageContentEl('#errorAlert').classList.remove('invisible', errorAlert());
+		//allows the user to imput both uppercase initials as well as lower case.
+	} else if (initialsRecord.match(/[[A-Za-z]/) === null) {
+		pageContentEl('#errorAlert p').innerHTML = "Please only enter letters for initials.";
+		pageContentEl('#errorAlert').classList.remove('invisible', errorAlert());
+	} else {
+		recordsArray.push({
+			"initialRecord": initialsRecord,
+			"score": score
+		});
+		//Stores highscores in local storage
+		localStorage.setItem('recordsArray', JSON.stringify(recordsArray));
+		pageContentEl('#highScores div').innerHTML = '';
+		onlyDisplaySection("#highScores");
+		recordsReset();
+		pageContentEl("#initials").value = '';
+	}
+};
+//Gives user the ability to clear the high scores
+var clearHighScores = function () {
+	recordsArray = [];
+	pageContentEl('#highScores div').innerHTML = "";
+	localStorage.removeItem('recordsArray');
+};
+//Resets quiz to the beginning
+var quizReset = function () {
+	time = initialTime;
+	score = 0;
+	quizCount = 0;
+	onlyDisplaySection("#intro");
+};
+//starts quiz
+var startQuiz = function () {
+	setQuestion();
+	onlyDisplaySection("#quizHolder");
+	clock = setInterval(timer, 1000);
+};
+//Allows user to see highscores list
+var viewHighScores = function (highscores) {
+	highscores.preventDefault();
+	clearInterval(clock);
+	pageContentEl('#time').innerHTML = 0;
+	time = initialTime;
+	score = 0;
+	quizCount = 0;
+	onlyDisplaySection("#highScores");
+	recordsReset();
+};
+//if a question is answered incorrectly, this function deducts 10 seconds.  If answered correctly, it adds a point.
+var scoreTimeAdjust = function () {
+	if (this.innerHTML.substring(3, this.length) === questions[quizCount].answer) {
+		score = score + 1;
+		quizCount = quizCount + 1;
+		quizUpdate("Correct");
+	} else {
+		time = time - 10;
+		quizCount = quizCount + 1;
+		quizUpdate("Incorrect");
+	}
+};
+
+
+var recordsReset = function () {
+	pageContentEl('#highScores div').innerHTML = "";
+	let i = 1;
+	recordsArray.sort((a, b) => b.score - a.score);
+	Array.from(recordsArray).forEach(check => {
+		var scores = document.createElement("div");
+		scores.innerHTML = i + ". " + check.initialRecord + " - " + check.score;
+		pageContentEl('#highScores div').appendChild(scores);
+		i = i + 1;
+	});
+	i = 0;
+	Array.from(answers).forEach(answer => {
+		answer.classList.remove('disable');
+	});
+};
+
+
+(localStorage.getItem('recordsArray')) ? recordsArray = JSON.parse(localStorage.getItem('recordsArray')) : recordsArray = [];
+
+
+Array.from(answers).forEach(check => { check.addEventListener('click', scoreTimeAdjust); });
+
+// Event Listeners 
+
+pageContentEl("#intro button").addEventListener("click", startQuiz);
+pageContentEl("#records button").addEventListener("click", enterInitials);
+pageContentEl("#clearScores").addEventListener("click", clearHighScores);
+pageContentEl("#reset").addEventListener("click", quizReset);
+pageContentEl("#scores").addEventListener("click", viewHighScores);
